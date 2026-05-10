dffee7dc60680ff2882532965f73208ceaddab62
giottin <giottin.sanglantina@gmail.com>
2026-05-10 17:55:56 +0200

    Add files via upload

---
+++ storage.js
@@ -0,0 +1,45 @@
+
+const supabase = window.supabase.createClient(
+'https://kijhnxehaxdjgvkddgyo.supabase.co',
+'sb_publishable_mAXF0I_pLCxjnDCoIvoMYA_crqYPypL'
+);
+
+async function saveCloud(state){
+
+try{
+
+await supabase
+.from('dashboard_data')
+.upsert({
+id:1,
+content:state,
+updated_at:new Date().toISOString()
+});
+
+}catch(e){
+console.error(e);
+}
+
+}
+
+async function loadCloud(){
+
+try{
+
+const response = await supabase
+.from('dashboard_data')
+.select('*')
+.eq('id',1)
+.single();
+
+if(response.data && response.data.content){
+return response.data.content;
+}
+
+}catch(e){
+console.error(e);
+}
+
+return null;
+
+}
